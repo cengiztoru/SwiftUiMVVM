b62f5892d31a7ad79e633912bcd40cf944e1020b
Cengiz TORU <cengiz.toru@tsoft.com.tr>
2021-08-30 05:23:20 +0300

    First Commit in SwiftUi with 💚

---
+++ Shared/CenTor_CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CenTor_CatsApp.swift
+//  Shared
+//
+//  Created by User on 30.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct CenTor_CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
